37b65159942c2d4c3e74b9b93b18216c1529e497
Manticore <ryanbaugh9@gmail.com>
2021-11-03 17:37:07 -0400

    header with js

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+// scroll animations
 let scroll =
 	window.requestAnimationFrame ||
 	function (callback) {
@@ -27,3 +28,14 @@ loop = () => {
 };
 
 loop();
+
+// change header on scroll
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+		header.classList.add("shrink");
+	} else {
+		header.classList.remove("shrink");
+	}
+});
